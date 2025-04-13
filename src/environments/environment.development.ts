9f4b2d57031ab8192784a2667339f733dad57976
Joel Jalixto <139259113+Jjalixto@users.noreply.github.com>
2025-04-12 22:23:21 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,8 +5,8 @@ export const environment = {
   companySlogan: 'Maneja tus gifs',
 
   //Apikeys
-  giphyApiKey: "uPPspnZ4opEncsBw7DntUmqvr3mOTAVW",
-  giphyUrl: "https://api.giphy.com/v1",
+  // giphyApiKey: "uPPspnZ4opEncsBw7DntUmqvr3mOTAVW",
+  // giphyUrl: "https://api.giphy.com/v1",
 
   //URLS
 };
